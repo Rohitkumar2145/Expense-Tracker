33484ae7a83f730e397956fa1ea7ee3c9309c0e1
RohitKumar Sharma <rohitsharmars2106@gmail.com>
2023-04-16 22:50:59 +0530

    Adding data via _props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date();
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 1475;
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{`${expenseDate.getFullYear()}-${expenseDate.getDate()}-${
-        expenseDate.getMonth() + 1
-      }`}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">₹{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">₹{props.amount}</div>
       </div>
     </div>
   );
