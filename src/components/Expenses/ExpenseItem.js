596c5140add5e17fa417b33fcb021bbe545390db
RohitKumar Sharma <rohitsharmars2106@gmail.com>
2023-04-19 00:59:07 +0530

    Adding Form input to list data

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,18 +4,12 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const call = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">₹{props.amount}</div>
-        <button onClick={call}>Click</button>
       </div>
     </Card>
   );
